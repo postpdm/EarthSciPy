516033a80007d51ff3f44cc461a9bbf3b6f83b02
yn-coder <yn.coder@gmail.com>
2016-12-22 21:26:24 +0300

    Wells field #2

--- exampes/wells_test.py
+++ exampes/wells_test.py
@@ -5,17 +5,32 @@ class BaseWell():
   wellhead_X = 0
   wellhead_Y = 0
   
-  def __init__(self):
-    pass
+  def __init__(self, arg_wellhead_X = 0, arg_wellhead_Y = 0 ):
+    self.wellhead_X = arg_wellhead_X
+    self.wellhead_Y = arg_wellhead_Y
    
 class Well(BaseWell):
   """Well class"""
+  #def __init__(self):
+  #  print("Well")
+  #  print( self.wellhead_X, self.wellhead_Y )
+
+class WellField():
+  """Well field class"""
+  Well_list = []
   def __init__(self):
-    print("Well")
-    print( self.wellhead_X, self.wellhead_Y )
+    pass
+
     
 print("Start")
 
-BW = Well()
+BW = Well( 11, -7 )
+
+WF = WellField()
+
+WF.Well_list.append( BW )
+
+for i in WF.Well_list:
+  print (i.wellhead_X, i.wellhead_Y)
 
 print("End")
