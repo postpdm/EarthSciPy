458f231ba7c9f0f6aa9ee9da957e8e79a0774c06
yn-coder <yn.coder@gmail.com>
2016-12-22 21:37:18 +0300

    Wells field #2 add_well method

--- exampes/wells_test.py
+++ exampes/wells_test.py
@@ -7,7 +7,7 @@ class BaseWell():
   
   def __init__(self, arg_wellhead_X = 0, arg_wellhead_Y = 0 ):
     self.wellhead_X = arg_wellhead_X
-    self.wellhead_Y = arg_wellhead_Y
+    self.wellhead_Y = arg_wellhead_Y    
    
 class Well(BaseWell):
   """Well class"""
@@ -20,15 +20,16 @@ class WellField():
   Well_list = []
   def __init__(self):
     pass
-
+  
+  def add_well(self, arg_well ):
+    self.Well_list.append( arg_well )
     
 print("Start")
 
-BW = Well( 11, -7 )
-
 WF = WellField()
 
-WF.Well_list.append( BW )
+WF.add_well( Well( 11, -7 ) )
+WF.add_well( Well( -99, 88 ) )
 
 for i in WF.Well_list:
   print (i.wellhead_X, i.wellhead_Y)
